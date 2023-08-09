b98440af87327480e520127c934d144b43570ff9
hectorHumanDev <113396173+MF-H0d@users.noreply.github.com>
2023-08-08 21:49:30 -0700

    New Kata Solution

---
+++ 8kyu/multiply.js
@@ -0,0 +1,15 @@
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//      a * b
+//   }
+
+//My Solutions 
+
+function multiply(a, b){
+    return a * b
+  }
+
+// ===============================================================================
+
+  let multiply = (a, b) => a * b;
