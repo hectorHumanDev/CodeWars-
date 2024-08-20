cc0f9a5ad523a50bfb1f3559f8a24dcbc4af27df
hectorHumanDev <113396173+MF-H0d@users.noreply.github.com>
2024-08-20 11:49:14 -0700

    New Kata Solution Added

--- 6kyu/YourOrderPlease.js
+++ 6kyu/YourOrderPlease.js
@@ -11,12 +11,13 @@
 
 // ========================solution================================
 
-function order(words){
-  
-    return words.split(' ').sort(function(a, b){
-        return a.match(/\d/) - b.match(/\d/);
-     }).join(' ');
-  }    
+function order(words) {
+  return words
+    .split(" ")
+    .sort(function (a, b) {
+      return a.match(/\d/) - b.match(/\d/);
+    })
+    .join(" ");
+}
 
-
-
+console.log(order("is2 Thi1s T4est 3a"));
